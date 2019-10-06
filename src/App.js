67cd9db77438a1eb01daeed3b9382135272ef6d9
unknown <gsingh181991>
2019-10-06 21:36:35 +0530

    Changed to react-mdl. Initialzed layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React Portfolio App
-        </p>
-      </header>
+    <div className="demo-big-content">
+      <Layout>
+        <Header title="My Portfolio" scroll>
+          <Navigation>
+            <a href="/">Resume</a>
+            <a href="/">About me</a>
+            <a href="/">Projects</a>
+            <a href="/">Contact</a>
+          </Navigation>
+        </Header>
+        <Drawer title="My Portfolio">
+          <Navigation>
+            <a href="/">Resume</a>
+            <a href="/">About me</a>
+            <a href="/">Projects</a>
+            <a href="/">Contact</a>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+        </Content>
+      </Layout>
     </div>
+
   );
 }
 
